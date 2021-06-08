bac9a2786797da267a00b7e9036ba095cba42cd3
Daniel Leal <37158127+daleal@users.noreply.github.com>
2021-06-08 18:40:07 -0400

    Add ids to responses (#42)
    * Add ids to responses
    
    * Modify endpoints
    
    * Fix documentation
    
    * Make ID attributes read only
    
    * Remove unnecessary schema
    
    * Fix incorrect description

--- backend/operations/serializers.py
+++ backend/operations/serializers.py
@@ -7,27 +7,32 @@ class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Store
         fields = ["id", "address", "zone"]
+        read_only_fields = ["id"]
 
 
 class ZoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Zone
         fields = ["id", "name"]
+        read_only_fields = ["id"]
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ["id", "store", "data", "created_at"]
+        read_only_fields = ["id"]
 
 
 class KPISerializer(serializers.ModelSerializer):
     class Meta:
         model = KPI
-        fields = ["store", "name", "value", "metadata", "category", "date"]
+        fields = ["id", "name", "value", "store", "category", "date", "metadata"]
+        read_only_fields = ["id"]
 
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceIndicator
-        fields = ["name", "value", "store", "amount_of_surveys", "date"]
+        fields = ["id", "name", "value", "store", "amount_of_surveys", "date"]
+        read_only_fields = ["id"]
