8d6c478b3432f7f36687ca1f2c1f528954faf5a7
Marialuisaclaro <42193915+Marialuisaclaro@users.noreply.github.com>
2021-07-06 16:38:07 -0400

    Feat/update seeds (#58)
    * first update
    
    * update service-indicators fixtures

--- scripts/fixture_generators/service_indicators2.py
+++ scripts/fixture_generators/service_indicators2.py
@@ -15,31 +15,37 @@ def to_fixtures(data, initial_id, model_name):
     return fixtures
 
 
-def generate_indicator(base, current_date, days):
+def generate_indicator(base, current_date, days, MEAN_VALUES, DESV_VALUES, MIN_VALUES):
     service_indicator = []
 
     for _ in range(days):
-        nps = random.randint(-5, 20)
-        values = [random.randint(0, 150) for _ in range(6)]
-        surveys = random.randint(0,25)
-        amount_values = [random.randint(0, surveys+1) for idx in range(7)]
+        values = [ random.randint(max(MIN_VALUES[idx],MEAN_VALUES[idx] - 
+        DESV_VALUES[idx]),MEAN_VALUES[idx]+DESV_VALUES[idx]) for idx in range(15)]
+
+
+        for idx in range(2,15,2):
+            values[idx] = min(values[0],values[idx])
+            if values[idx] == 0:
+                values[idx-1]=0
+
+        
 
         service_indicator.append({
-            "nps": nps,
-            "amount_nps": amount_values[0],
-            "experience": values[0],
-            "amount_experience": amount_values[1],
-            "kindness": values[1],
-            "amount_kindness": amount_values[2],
-            "waiting_time": values[2],
-            "amount_waiting_time": amount_values[3],
-            "speed": values[3],
-            "amount_speed": amount_values[4],
-            "quality": values[4],
-            "amount_quality": amount_values[5],
-            "bathroom": values[5],
-            "amount_bathroom": amount_values[6],
-            "amount_of_surveys": surveys,
+            "amount_of_surveys": values[0],          
+            "nps": values[1],
+            "amount_nps": values[2],
+            "experience": values[3],
+            "amount_experience": values[4],
+            "kindness": values[5],
+            "amount_kindness": values[6],
+            "waiting_time": values[7],
+            "amount_waiting_time": values[8],
+            "speed": values[9],
+            "amount_speed": values[10],
+            "quality": values[11],
+            "amount_quality": values[12],
+            "bathroom": values[13],
+            "amount_bathroom": values[14],
             "date": current_date.strftime("%Y-%m-%d"),
             **copy.deepcopy(base),
         })
@@ -49,12 +55,15 @@ def generate_indicator(base, current_date, days):
     return service_indicator
 
 
-def generate_indicators(base, current_date, days):
+def generate_indicators(base, current_date, days, MEAN_VALUES, DESV_VALUES, MIN_VALUES):
     service_indicators = [
         *generate_indicator(
             base,
             current_date,
-            days
+            days,
+            MEAN_VALUES,
+            DESV_VALUES,
+            MIN_VALUES
         ),
     ]
 
@@ -77,17 +86,26 @@ if __name__ == "__main__":
     INITIAL_DATE = datetime.strptime("2021-06-01", "%Y-%m-%d")
 
 
+    MEAN_VALUES = [7,1,2,14,2,14,2,14,2,10,1,10,1,10,1]
+    DESV_VALUES= [2,1,2,10,1,10,2,10,2,6,1,6,1,7,1]
+    MIN_VALUES= [1,-3,1,1,1,1,0,1,1,1,0,1,0,1,0]
 
     FIRST_SERVICE_INDICATORS = generate_indicators(
         FIRST_STORE_BASE,
         INITIAL_DATE,
-        100
+        100,
+        MEAN_VALUES,
+        DESV_VALUES,
+        MIN_VALUES
     )
 
     SECOND_SERVICE_INDICATORS = [*generate_indicators(
         SECOND_STORE_BASE,
         INITIAL_DATE,
-        100
+        100,
+        MEAN_VALUES,
+        DESV_VALUES,
+        MIN_VALUES
     )]
 
     SERVICE_INDICATORS = [*FIRST_SERVICE_INDICATORS, *SECOND_SERVICE_INDICATORS]
