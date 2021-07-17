c3046f3e7858157c6b2f4b8d90f4eca73ad56124
Marialuisaclaro <42193915+Marialuisaclaro@users.noreply.github.com>
2021-07-17 18:52:09 -0400

    feat: updateSI script (#61)
    * feat: updateSI script
    
    * update kpi script
    
    * fix linters
    
    * fix pr

--- scripts/create_endpoints/script_service_indicators.py
+++ scripts/create_endpoints/script_service_indicators.py
@@ -1,7 +1,7 @@
 import requests
 import csv
 
-API_KEY = '9yydziuU.WptcL4vglt0VHmyJngoKiZlN8Qz5ZLQZ'
+API_KEY = 'ROTci9Go.VKjLkVI31KNj4q0lELQ9pC591VMr39fq'
 file_name = 'service-indicators.csv'
 
 headers = {
@@ -16,22 +16,35 @@ with open(file_name) as csv_file:
   for row in csv_reader:
     if lines== 0:
       lines+=1
-      print(f'Column names are {", ".join(row)}')
+      #print(f'Column names are {", ".join(row)}')
     else:
       lines+=1
       params = {
-        "name": row[0],
-        "value": float(row[1]),
-        "store": int(row[2]),
+        "date": row[0],
+        "store": int(row[1]),
         "amount_of_surveys": int(row[3]),
-        "date": row[4],
+        "nps": int(row[4]) if int(row[5]) else 0,
+        "amount_nps": int(row[5]),
+        "experience": int(row[6]) if int(row[7]) else 0,
+        "amount_experience": int(row[7]),
+        "kindness": int(row[8]) if int(row[9]) else 0,
+        "amount_kindness": int(row[9]),
+        "waiting_time": int(row[10]) if int(row[11]) else 0,
+        "amount_waiting_time": int(row[11]),
+        "speed": int(row[12]) if int(row[13]) else 0,
+        "amount_speed": int(row[13]),
+        "quality": int(row[14]) if int(row[15]) else 0,
+        "amount_quality": int(row[15]),
+        "bathroom": int(row[16]) if int(row[17]) else 0,
+        "amount_bathroom": int(row[17]),
       }
-      response = requests.put(url, data=params, headers=headers,)
+      response = requests.put(url, data=params, headers=headers)
       if response.status_code == 201 or response.status_code == 200 :
         continue
       else:
-        res = response.json()
-        print(res)
+        print("Some Error")
+        #res = response.json()
+        print(response)
         break
   print(f'Processed {lines-1} Service Indicators')
 
